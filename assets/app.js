04c6b572b7035556982beea26c5ccb2370353046
Piotr Szewczyk <pszewczykpl@gmail.com>
2024-12-30 00:56:38 +0100

    feat(all): add header app

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,11 @@
-import 'flowbite/dist/flowbite.min.css';
-import './styles/app.css';
-
-import './bootstrap.js';
-import 'flowbite';
-
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './styles/app.css';
+import 'flowbite/dist/flowbite.min.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import './bootstrap.js';
+import 'flowbite';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class',
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
